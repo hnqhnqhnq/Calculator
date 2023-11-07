1677462ce2583e82701839aae834a37dbe551fad
Hnq <hancu345@gmail.com>
2023-11-07 11:59:05 +0200

    minor changes in css and js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const buttons = document.querySelectorAll(".button");
 const textBox = document.querySelector(".calculator-textbox");
-let value = 0;
 let numberPoint = 0;
-let inlineValue = 0;
+let result = 0;
 
-function updateValue(button){
+function calculateValue(button){
+   
+}
+
+function updateText(button){
     textBox.value = textBox.value + button.textContent;
-    value = parseFloat(textBox.value);
     textBox.scrollLeft = textBox.scrollWidth;
 }
 
 function deleteContent(){
     textBox.value="";
-    value = 0;
     numberPoint = 0;
-    inlineValue = 0;
+    result = 0;
 }
 
 function writeContent(button){
     if(button.textContent >= "0" && button.textContent <= "9")
-        updateValue(button);
+        updateText(button);
     else if(button.textContent == "." && numberPoint == 0 
         && textBox.value[textBox.value.length-1]>= "0" && textBox.value[textBox.value.length-1] <="9"){
-            numberPoint++;
-            updateValue(button);
+        numberPoint++;
+        updateText(button);
     }
-    else{
-        
-    }
+    else
+        calculateValue(button);
 }
 
 buttons.forEach((button) => {
